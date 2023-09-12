b930e47f1121198fdbec0fbbe096c6ec531a0b6c
jlydikse <lyd15002@byui.edu>
2023-09-11 19:14:41 -0600

    described my process without youtube link

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,27 @@
+# Overview
+
+# In this program I wanted to see if I could get a helloworld program
+# set up within my IDE and also sync it with my GitHub repository
+# I want to solidify this learning for myself so that I can confidently
+# Build a github portfolio for my future projects
+
+
+
+{Provide a link to your YouTube demonstration.  It should be a one minute demo of the software running and a walkthrough of the code.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+# I used VScode as my IDE
+# I used Git to push and commit my code to my github repository
+# I used Python within VScode as my programming language. This is the language I am most familiar with
+
+# Useful Websites
+
+# Helpful websites used:
+
+# [Github](https://docs.github.com/en/get-started/quickstart/hello-world)
+# [freecodecamp](https://www.freecodecamp.org/news/introduction-to-git-and-github/)
+
 print("Hello World")
