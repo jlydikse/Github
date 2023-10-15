f2b8915e20ad04e07072f9dd4bc88e51187b4704
jlydikse <lyd15002@byui.edu>
2023-10-14 20:02:45 -0600

    saved

--- djangoQS/myproject/myapp/views.py
+++ djangoQS/myproject/myapp/views.py
@@ -1,56 +1,16 @@
-from django.db import models
-from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
-from django.contrib import messages
-from django.http import HttpResponse
+from django.db import models
+
 
 # Create your views here.
 def myview(request):
     return render(request, "index.html")
 
-def UserRegister(request):
-    if request.method=="POST":   
-        username = request.POST['username']
-        email = request.POST['email']
-        first_name=request.POST['first_name']
-        last_name=request.POST['last_name']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-        user = User.objects.create_user(username, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
-        return render(request, 'login.html')        
-    return render(request, "register.html")
+class Thread(models.Model):
+    title = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-def UserLogin(request):
-    if request.method=="POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Successfully Logged In")
-            return redirect("/profile")
-        else:
-            return HttpResponse("User not Found")
-        alert = True
-        return render(request, 'login.html', {'alert':alert})            
-    return render(request, "login.html")
-
-def myprofile(request):
-    if request.method=="POST":
-        user = request.user    
-        profile = Profile(user=user)
-        profile.save()
-        form = ProfileForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            obj = form.instance
-            return render(request, "profile.html",{'obj':obj})
-    else:
-        form=ProfileForm()
-    return render(request, "profile.html", {'form':form})
-
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
